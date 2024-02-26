eaeb2a5e572b21129655bed3135d77ff99928ae3
Mitchel <mitchel_jager@hotmail.com>
2024-02-27 00:12:06 +0100

    feat: Add custom fonts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<script>
+  import "$lib/scss/app.scss"
+  import "$lib/scss/fonts.scss"
+</script>
+
+<div class="layout">
+  <aside class="sidebar">
+    <header class="header">
+      <img src="/images/logo.png" alt="Helldivers 2" />
+
+      <p>Helldivers Training Manual</p>
+    </header>
+  </aside>
+
+  <article class="article">
+    <slot />
+  </article>
+</div>
+
+<style lang="scss">
+  .layout {
+    display: grid;
+    grid-template-columns: 25rem auto;
+    grid-template-rows: 10rem auto;
+    height: 100vh;
+  }
+
+  .header {
+    font-size: 1.75rem;
+    font-family: "Jockey One";
+    text-align: center;
+
+    p {
+      margin: $margin * 0.25 0 0;
+      color: $primary;
+    }
+  }
+
+  .sidebar {
+    height: 100vh;
+    padding: $margin;
+    border-right: 5px solid $bg-dark;
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  .article {
+    padding: $margin;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="m-0">Welcome to SvelteKit</h1>
+<h2 class="m-0">Welcome to SvelteKit</h2>
+<h3 class="m-0">Welcome to SvelteKit</h3>
+<h4 class="m-0">Welcome to SvelteKit</h4>
+<h5 class="m-0">Welcome to SvelteKit</h5>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quoniamque in iis rebus, quae neque in virtutibus sunt neque in vitiis, est tamen quiddam, quod usui possit esse, tollendum id non est. Sit hoc ultimum bonorum, quod nunc a me defenditur; Sunt enim prima elementa naturae, quibus auctis vírtutis quasi germen efficitur. Hoc uno captus Erillus scientiam summum bonum esse defendit nec rem ullam aliam per se expetendam. Idem etiam dolorem saepe perpetiuntur, ne, si id non faciant, incidant in maiorem. </p>
+
+<p>Sin te auctoritas commovebat, nobisne omnibus et Platoni ipsi nescio quem illum anteponebas? Nam illud vehementer repugnat, eundem beatum esse et multis malis oppressum. Cur post Tarentum ad Archytam? Bork <mark>Mihi, inquam, qui te id ipsum rogavi?</mark> Ex quo magnitudo quoque animi existebat, qua facile posset repugnari obsistique fortunae, quod maximae res essent in potestate sapientis. Atqui pugnantibus et contrariis studiis consiliisque semper utens nihil quieti videre, nihil tranquilli potest. Modo etiam paulum ad dexteram de via declinavi, ut ad Pericli sepulcrum accederem. <mark>Quo modo autem optimum, si bonum praeterea nullum est?</mark> Scisse enim te quis coarguere possit? Quod cum dixissent, ille contra. Huius, Lyco, oratione locuples, rebus ipsis ielunior. Vide, ne etiam menses! nisi forte eum dicis, qui, simul atque arripuit, interficit. Nullum inveniri verbum potest quod magis idem declaret Latine, quod Graece, quam declarat voluptas. </p>
+
+<p><mark>Bork</mark> <mark>Quippe: habes enim a rhetoribus;</mark> </p>
+
+<p>Huic ego, si negaret quicquam interesse ad beate vivendum quali uteretur victu, concederem, laudarem etiam; Quocirca intellegi necesse est in ipsis rebus, quae discuntur et cognoscuntur, invitamenta inesse, quibus ad discendum cognoscendumque moveamur. Quod maxime efficit Theophrasti de beata vita liber, in quo multum admodum fortunae datur. Utrum igitur tibi non placet, inquit, virtutisne tantam esse vim, ut ad beate vivendum se ipsa contenta sit? Quamquam scripsit artem rhetoricam Cleanthes, Chrysippus etiam, sed sic, ut, si quis obmutescere concupierit, nihil aliud legere debeat. <b>Philosophi autem in suis lectulis plerumque moriuntur.</b> Aut, Pylades cum sis, dices te esse Orestem, ut moriare pro amico? Sed ego in hoc resisto; </p>
+
+<p><a href="http://loripsum.net/" target="_blank">Quo modo?</a> Neque solum ea communia, verum etiam paria esse dixerunt. Non pugnem cum homine, cur tantum habeat in natura boni; Sin autem est in ea, quod quidam volunt, nihil impedit hanc nostram comprehensionem summi boni. </p>
+
+<p><a href="http://loripsum.net/" target="_blank">Tubulo putas dicere?</a> Qui non moveatur et offensione turpitudinis et comprobatione honestatis? Sed quid minus probandum quam esse aliquem beatum nec satis beatum? Immo istud quidem, inquam, quo loco quidque, nisi iniquum postulo, arbitratu meo. Eorum enim est haec querela, qui sibi cari sunt seseque diligunt. Commoda autem et incommoda in eo genere sunt, quae praeposita et reiecta diximus; Mihi vero, inquit, placet agi subtilius et, ut ipse dixisti, pressius. Quae quo sunt excelsiores, eo dant clariora indicia naturae. </p>
+
+<p>Positum est a nostris in iis esse rebus, quae secundum naturam essent, non dolere; <a href="http://loripsum.net/" target="_blank">Egone quaeris, inquit, quid sentiam?</a> </p>
+
+<p>Sed utrum hortandus es nobis, Luci, inquit, an etiam tua sponte propensus es? De malis autem et bonis ab iis animalibus, quae nondum depravata sint, ait optime iudicari. Certe nihil nisi quod possit ipsum propter se iure laudari. Nam haec ipsa mihi erunt in promptu, quae modo audivi, nec ante aggrediar, quam te ab istis, quos dicis, instructum videro. <a href="http://loripsum.net/" target="_blank">Urgent tamen et nihil remittunt.</a> <i>Quid de Pythagora?</i> Non prorsus, inquit, omnisque, qui sine dolore sint, in voluptate, et ea quidem summa, esse dico. </p>
+
+<p>Cuius etiam illi hortuli propinqui non memoriam solum mihi afferunt, sed ipsum videntur in conspectu meo ponere. Quid loquor de nobis, qui ad laudem et ad decus nati, suscepti, instituti sumus? Vide, ne etiam menses! nisi forte eum dicis, qui, simul atque arripuit, interficit. Hoc positum in Phaedro a Platone probavit Epicurus sensitque in omni disputatione id fieri oportere. Sed emolumenta communia esse dicuntur, recte autem facta et peccata non habentur communia. Praeclare enim Plato: Beatum, cui etiam in senectute contigerit, ut sapientiam verasque opiniones assequi possit. <b>Quis enim redargueret?</b> Itaque dicunt nec dubitant: mihi sic usus est, tibi ut opus est facto, fac. Plane idem, inquit, et maxima quidem, qua fieri nulla maior potest. Ita fit cum gravior, tum etiam splendidior oratio. </p>
+
+<p>Minime id quidem, inquam, alienum, multumque ad ea, quae quaerimus, explicatio tua ista profecerit. <b>Hunc vos beatum;</b> Duo Reges: constructio interrete. Quod si ita se habeat, non possit beatam praestare vitam sapientia. Atque haec contra Aristippum, qui eam voluptatem non modo summam, sed solam etiam ducit, quam omnes unam appellamus voluptatem. Virtutibus igitur rectissime mihi videris et ad consuetudinem nostrae orationis vitia posuisse contraria. Deinde non quaerimus, quid obscuretur aut intereat, quia sit admodum parvum, sed quid tale sit, ut expleat summam. Non igitur bene. Bonum negas esse divitias, praeposìtum esse dicis? </p>
 
 <style lang="scss">
   p a {
