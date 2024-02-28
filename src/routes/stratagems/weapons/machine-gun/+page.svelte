a362298ab01a75d709761f7f5ea59156dbb69161
Mitchel <mitchel_jager@hotmail.com>
2024-02-28 03:16:36 +0100

    chore: Add more content

---
+++ src/routes/stratagems/weapons/machine-gun/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Stratagem from "$lib/components/Stratagem.svelte"
+</script>
+
+<h1>MG-43 Machine gun</h1>
+
+<p>Behold, soldier, the mighty MG-43 Machine Gun - a weapon fit for heroes and hotheads alike. This trusty companion is your ticket to glory on the battlefield, but beware, for with great power comes great responsibility (and a hefty kickback).</p>
+
+<p>Now, let's talk tactics. The MG-43 is your go-to for those "oh crap" moments when you're faced with bug breach hordes or pesky large enemies. But here's the catch - <strong>ammo is scarce</strong>, so it's like momma always said: "Use it wisely, or you'll be walking home."</p>
+
+<p>When it comes to accuracy and control, remember this mantra: "Stationary is the new black." <strong>Plant your feet</strong>, soldier, and watch as your shots hit the mark with precision. And if you really want to show those bugs who's boss, get low - <strong>crouch for stability</strong> or go full commando and hit the dirt for maximum accuracy.</p>
+
+<p>Now, about armor penetration - the MG-43 may not cut through armor like a hot knife through butter, but it'll sure give those enemies a run for their money. So when you see a tough bug or a sturdy automaton, don't be afraid to lay down some suppressive fire and watch those bullets find their mark.</p>
+
+<div class="mt-1">
+	<Stratagem sequence={["down", "left", "down", "up", "right"]} />
+</div>
