b96d12b45132906c52d4e6ac3f64be6094f0ac7a
Mitchel <mitchel_jager@hotmail.com>
2024-02-29 21:01:36 +0100

    chore: Add content for all weapon stratagems

--- src/lib/data/stratagems.js
+++ src/lib/data/stratagems.js
@@ -1,5 +1,5 @@
 export const stratagems = [{
-  category: "Weapons",
+  category: "Support Weapons",
   items: [{
     name: "Machine Gun",
     sequence: ["down", "left", "down", "up", "right"],
@@ -13,179 +13,343 @@ export const stratagems = [{
     name: "Anti-Material Rifle",
     sequence: ["down", "left", "right", "up", "down"],
     content: `
-      <p>The Anti-Materiel Rifle is the ultimate precision instrument for dealing with those big, bad enemies that think they're invincible. This beast of a sniper rifle is your solution when you need to reach out and touch someone – hard.</p>
+      <p>The Anti-Materiel Rifle is the ultimate precision instrument for dealing with those big, bad enemies that think they're invincible. This beast of a sniper rifle is your solution when you need to reach out and touch someone - hard.</p>
       <p>Imagine the scenario: you're face-to-face with a towering Automaton Hulk, its armored plates gleaming with menace. With the Anti-Materiel Rifle in hand, you take aim, squeeze the trigger, and watch as two shots are all it takes to dismantle that mechanical monstrosity.</p>
-      <p>But here's the kicker – this ain't your granddaddy's hunting rifle. The Anti-Materiel Rifle can only be aimed down sights, meaning you'll need a steady hand and a keen eye to make every shot count. Lucky for you, this baby comes equipped with multiple zoom levels, so you can pick off targets from afar with surgical precision.</p>
+      <p>But here's the kicker - this ain't your granddaddy's hunting rifle. The Anti-Materiel Rifle can only be aimed down sights, meaning you'll need a steady hand and a keen eye to make every shot count. Lucky for you, this baby comes equipped with multiple zoom levels, so you can pick off targets from afar with surgical precision.</p>
       <p>And speaking of precision, here's a pro tip: stay low and steady to maximize stability and reduce recoil. Whether you're crouched behind cover or prone in the dirt, taking the time to line up your shot will ensure that each round finds its mark with deadly accuracy.</p>
       <p>So, soldier, when the chips are down and the Automatons are closing in, trust in the power of the Anti-Materiel Rifle to deliver justice from a distance. With its unmatched stopping power and pinpoint accuracy, liberty is just a well-placed shot away.</p>
     `
   }, {
     name: "Stalwart",
-    sequence: ["down", "left", "down", "up", "up", "left"]
+    sequence: ["down", "left", "down", "up", "up", "left"],
+    content: `
+      <p>The M-105 Stalwart is a compact and nimble machine gun designed for soldiers who value agility and versatility on the battlefield. Unlike its heavier counterparts, the Stalwart sacrifices raw power for ease of use, making it the perfect choice for those who need to stay mobile in the heat of combat.</p>
+      <p>The Stalwart truly shines in its agility. Unlike other machine guns, the Stalwart allows you to reload on the move, ensuring that you're always ready to keep the pressure on the enemy. And with the ability to adjust the RPM between slower and faster rates, you have the flexibility to adapt your firepower to the situation at hand.</p>
+      <p>While the Stalwart may not pack the same punch as its heavier counterparts, it more than makes up for it with its nimble handling and rapid reloads. Whether you're facing hordes of smaller enemies or need to maintain constant suppression fire, the M-105 Stalwart provides the firepower and agility you need to get the job done.</p>
+    `
   }, {
     name: "Expendable Anti-Tank",
-    sequence: ["down", "down", "left", "up", "right"]
+    sequence: ["down", "down", "left", "up", "right"],
+    content: `
+      <p>The EAT-17 Expandable Anti-Tank is a single-use weapon specially designed to wreak havoc on vehicle armor targets.</p>
+      <p>When facing formidable armored foes, the EAT-17 Expandable Anti-Tank becomes your ultimate ally. The Stratagem delivers two rocket launchers, each capable of delivering a devastating blow, this weapon is designed to tear through the toughest of armor with ease.</p>
+      <p>But here's the catch - the EAT-17 is a one-shot wonder. Once you've unleashed its payload, the launcher is discarded, so make every shot count. Whether you're facing off against hulking Terminids or menacing Automatons, the EAT-17 is your ticket to leveling the playing field and coming out on top.</p>
+    `
   }, {
     name: "Recoilless Rifle",
-    sequence: ["down", "left", "right", "right", "left"]
+    sequence: ["down", "left", "right", "right", "left"],
+    content: `
+      <p>Behold, the GR-8 Recoilless Rifle - a weapon so mighty, it'll make even the toughest armored vehicles quake in fear. This bad boy is your ticket to turning enemy armor into scrap metal faster than you can say "reload."</p>
+      <p>With its single, devastating rocket, the GR-8 is the ultimate choice for obliterating those pesky big foes on the battlefield. But beware, soldier, reloading this beast is no walk in the park - unless you've got a trusty buddy to lend a hand, of course. Introducing the support backpack - the unsung hero of the GR-8's reload process. While going solo might leave you twiddling your thumbs, having a teammate grab your ammo backpack can turn reloading from a chore into a breeze faster than you can say "teamwork."</p>
+      <p>So, when the enemy rolls out the big guns, don't just stand there - grab your GR-8, slap on that support backpack, and show 'em what you're made of. With the power of friendship and a whole lot of rockets, victory is just a blast away.</p>
+    `
   }, {
     name: "Flamethrower",
-    sequence: ["down", "left", "up", "down", "up"]
+    sequence: ["down", "left", "up", "down", "up"],
+    content: `
+      <p>The FLAM-40 Flamethrower is a weapon of fiery destruction, perfect for scorching enemies at close range. With its incendiary power, this weapon doesn't just burn targets - it turns them into crispy critters faster than you can say "extra crispy."</p>
+      <p>But beware, soldier, the FLAM-40 doesn't discriminate - it'll ignite anything and everything in its path, including terrain and, yes, even your flammable teammates. So, wield this weapon with caution and keep your distance from anything you'd rather not see engulfed in flames.</p>
+      <p>So, when the flames of democracy meet the inferno of the FLAM-40 Flamethrower, even the most ardent enemies of liberty will find themselves roasted to a crisp. Remember, nothing says "freedom" like a well-cooked bug!</p>
+    `
   }, {
     name: "Autocannon",
-    sequence: ["down", "right", "left", "down", "down", "up", "up", "right"]
+    sequence: ["down", "right", "left", "down", "down", "up", "up", "right"],
+    content: `
+    <p>The AC-8 Autocannon is a fully-automatic cannon designed to rain destruction upon light vehicle armor with ruthless efficiency. With its rapid-fire capability, this weapon turns enemy vehicles into scrap metal faster than you can say "reload."</p>
+    <p>The AC-8 comes equipped with a support backpack, allowing your trusty teammates to lend a hand and make reloading a breeze. With their help, you'll be back in action and ready to unleash another barrage of bullets in no time.</p>
+    <p>One advantage of the AC-8 is its ability to destroy objectives from a considerable distance away. Whether you're taking out enemy installations or blasting through fortified defenses, this cannon has got you covered.</p>
+    <p>But wait, there's more! The AC-8 isn't just for taking down big targets - it's also a force to be reckoned with against hordes of smaller enemies. With its high rate of fire, this cannon chews through anything foolish enough to stand in its way, leaving nothing but destruction in its wake.</p>
+    `
   }, {
     name: "Railgun",
-    sequence: ["down", "right", "left", "down", "down", "up", "left", "down", "right"]
+    sequence: ["down", "right", "left", "down", "down", "up", "left", "down", "right"],
+    content: `
+      <p>The RS-422 Railgun isn't your grandpa's pea shooter - it's a cutting-edge piece of experimental tech that's so hot, it makes the sun jealous. This baby is all about one thing and one thing only: blasting through armor like it's going out of style.</p>
+      <p>But hold your horses, soldier - each shot needs a little love and care, so make sure you're cozy with that trigger before you pull it. Remember, precision is key - we're talking about turning enemy armor into modern art here. This bad boy fires a single, supercharged shot at a time, cutting through armor plates like a hot knife through butter. It's like playing a game of "pin the tail on the donkey," except the donkey is an enemy tank and the tail is a high-velocity projectile.</p>
+      <p>But wait, there's more! The RS-422 comes with a little something we like to call the "unsafe" mode. Sure, it's risky business, but who doesn't love a little danger with their morning coffee? Just remember: hold that trigger down for too long and you'll be painting the walls with more than just enemy blood.</p>
+    `
   }, {
     name: "Spear",
-    sequence: ["down", "down", "up", "down", "down"]
+    sequence: ["down", "down", "up", "down", "down"],
+    content: `
+      <p>The FAF-14 Spear is the pinnacle of anti-tank warfare, a homing missile that locks onto its target with deadly precision before unleashing its destructive payload. With its auto-lock-on system, this weapon is your best friend when it comes to taking down large and armored enemies, no matter how fast they're moving.</p>
+      <p>The FAF-14 Spear comes equipped with a handy backpack, allowing your trusty teammates to lend a hand and make reloading a breeze. With their support, you'll be ready to lock and load in no time, raining missiles down on your enemies like a force of nature.</p>
+    `
   }]
 }, {
   category: "Orbital",
   items: [{
     name: "Gatling Barrage",
-    sequence: ["right", "down", "left", "up", "up"]
+    sequence: ["right", "down", "left", "up", "up"],
+    content: `
+
+    `
   }, {
     name: "Airburst Strike",
-    sequence: ["right", "right", "right"]
+    sequence: ["right", "right", "right"],
+    content: `
+
+    `
   }, {
     name: "120MM HE Barrage",
-    sequence: ["right", "down", "down", "left", "down", "right", "down", "down"]
+    sequence: ["right", "down", "down", "left", "down", "right", "down", "down"],
+    content: `
+
+    `
   }, {
     name: "380MM HE Barrage",
-    sequence: ["right", "down", "down", "up", "up", "left", "down", "down", "down"]
+    sequence: ["right", "down", "down", "up", "up", "left", "down", "down", "down"],
+    content: `
+
+    `
   }, {
     name: "Walking Barrage",
-    sequence: ["right", "down", "right", "down", "right", "down"]
+    sequence: ["right", "down", "right", "down", "right", "down"],
+    content: `
+
+    `
   }, {
     name: "Laser Strike",
-    sequence: ["right", "up", "left", "up", "right", "left"]
+    sequence: ["right", "up", "left", "up", "right", "left"],
+    content: `
+
+    `
   }, {
     name: "Railcannon Strike",
-    sequence: ["right", "down", "up", "down", "left"]
+    sequence: ["right", "down", "up", "down", "left"],
+    content: `
+
+    `
   }]
 }, {
   category: "Hangar",
   items: [{
     name: "Eagle Strafing Run",
-    sequence: ["up", "right", "right"]
+    sequence: ["up", "right", "right"],
+    content: `
+
+    `
   }, {
     name: "Eagle Airstrike",
-    sequence: ["up", "right", "down", "right"]
+    sequence: ["up", "right", "down", "right"],
+    content: `
+
+    `
   }, {
     name: "Eagle Cluster Bomb",
-    sequence: ["up", "right", "down", "down", "right", "down"]
+    sequence: ["up", "right", "down", "down", "right", "down"],
+    content: `
+
+    `
   }, {
     name: "Eagle Napalm Airstrike",
-    sequence: ["up", "right", "down", "up"]
+    sequence: ["up", "right", "down", "up"],
+    content: `
+
+    `
   }, {
     name: "Jump Pack",
-    sequence: ["down", "up", "up", "down", "up"]
+    sequence: ["down", "up", "up", "down", "up"],
+    content: `
+
+    `
   }, {
     name: "Eagle Smoke Strike",
-    sequence: ["up", "right", "up", "down"]
+    sequence: ["up", "right", "up", "down"],
+    content: `
+
+    `
   }, {
     name: "Eagle 110MM Rocket Pods",
-    sequence: ["up", "down", "up", "left"]
+    sequence: ["up", "down", "up", "left"],
+    content: `
+
+    `
   }, {
     name: "Eagle 500KG Bomb",
-    sequence: ["up", "left", "down", "down", "down"]
+    sequence: ["up", "left", "down", "down", "down"],
+    content: `
+
+    `
   }]
 }, {
   category: "Bridge",
   items: [{
     name: "Orbital Precision Strikes",
-    sequence: ["right", "right", "up"]
+    sequence: ["right", "right", "up"],
+    content: `
+
+    `
   }, {
     name: "Orbital Gas Strike",
-    sequence: ["right", "right", "down", "right"]
+    sequence: ["right", "right", "down", "right"],
+    content: `
+
+    `
   }, {
     name: "Orbital EMS Strike",
-    sequence: ["right", "right", "left", "down"]
+    sequence: ["right", "right", "left", "down"],
+    content: `
+
+    `
   }, {
     name: "Orbital Smoke Strike",
-    sequence: ["right", "right", "down", "up"]
+    sequence: ["right", "right", "down", "up"],
+    content: `
+
+    `
   }, {
     name: "HMG Emplacement",
-    sequence: ["up", "down", "left", "right", "right", "left"]
+    sequence: ["up", "down", "left", "right", "right", "left"],
+    content: `
+
+    `
   }, {
     name: "Shield Generator Relay",
-    sequence: ["down", "up", "left", "right", "left", "down"]
+    sequence: ["down", "up", "left", "right", "left", "down"],
+    content: `
+
+    `
   }, {
     name: "Tesla Tower",
-    sequence: ["down", "up", "right", "up", "left", "right"]
+    sequence: ["down", "up", "right", "up", "left", "right"],
+    content: `
+
+    `
   }]
 }, {
   category: "Engineering Bay",
   items: [{
     name: "Anti-Personnel Minefield",
-    sequence: ["down", "left", "down", "up", "right"]
+    sequence: ["down", "left", "down", "up", "right"],
+    content: `
+
+    `
   }, {
     name: "Supply Pack",
-    sequence: ["down", "left", "down", "up", "up", "down"]
+    sequence: ["down", "left", "down", "up", "up", "down"],
+    content: `
+
+    `
   }, {
     name: "Grenade Launcher",
-    sequence: ["down", "left", "down", "up", "left", "down", "down"]
+    sequence: ["down", "left", "down", "up", "left", "down", "down"],
+    content: `
+
+    `
   }, {
     name: "Laser Cannon",
-    sequence: ["down", "left", "down", "up", "left"]
+    sequence: ["down", "left", "down", "up", "left"],
+    content: `
+
+    `
   }, {
     name: "Incendiary Mines",
-    sequence: ["down", "left", "left", "down"]
+    sequence: ["down", "left", "left", "down"],
+    content: `
+
+    `
   }, {
     name: "Guard Dog Rover",
-    sequence: ["down", "left", "down", "up", "left", "down", "down"]
+    sequence: ["down", "left", "down", "up", "left", "down", "down"],
+    content: `
+
+    `
   }, {
     name: "Ballistic Shield Backpack",
-    sequence: ["down", "left", "up", "up", "right"]
+    sequence: ["down", "left", "up", "up", "right"],
+    content: `
+
+    `
   }, {
     name: "Arc Thrower",
-    sequence: ["down", "right", "up", "left", "down"]
+    sequence: ["down", "right", "up", "left", "down"],
+    content: `
+
+    `
   }, {
     name: "Shield Generator Pack",
-    sequence: ["down", "up", "left", "down", "right", "right"]
+    sequence: ["down", "up", "left", "down", "right", "right"],
+    content: `
+
+    `
   }]
 }, {
   category: "Robotics Workshop",
   items: [{
     name: "Machine Gun Sentry",
-    sequence: ["down", "up", "right", "right", "up"]
+    sequence: ["down", "up", "right", "right", "up"],
+    content: `
+
+    `
   }, {
     name: "Gatling Sentry",
-    sequence: ["down", "up", "right", "left", "down"]
+    sequence: ["down", "up", "right", "left", "down"],
+    content: `
+
+    `
   }, {
     name: "Mortar Sentry",
-    sequence: ["down", "up", "right", "right", "down"]
+    sequence: ["down", "up", "right", "right", "down"],
+    content: `
+
+    `
   }, {
     name: "Guard Dog",
-    sequence: ["down", "up", "left", "up", "right", "down"]
+    sequence: ["down", "up", "left", "up", "right", "down"],
+    content: `
+
+    `
   }, {
     name: "Autocannon Sentry",
-    sequence: ["down", "up", "right", "up", "left", "up"]
+    sequence: ["down", "up", "right", "up", "left", "up"],
+    content: `
+
+    `
   }, {
     name: "Rocket Sentry",
-    sequence: ["down", "up", "right", "right", "left"]
+    sequence: ["down", "up", "right", "right", "left"],
+    content: `
+
+    `
   }, {
     name: "EMS Mortar Sentry",
-    sequence: ["down", "down", "up", "up", "left"]
+    sequence: ["down", "down", "up", "up", "left"],
+    content: `
+
+    `
   }]
 }, {
   category: "Mission Stratagems",
   items: [{
     name: "Reinforce",
-    sequence: ["up", "down", "right", "left", "up"]
+    sequence: ["up", "down", "right", "left", "up"],
+    content: `
+
+    `
   }, {
     name: "SOS Beacon",
-    sequence: ["up", "down", "right", "up"]
+    sequence: ["up", "down", "right", "up"],
+    content: `
+
+    `
   }, {
     name: "Super Earth Flag",
-    sequence: ["down", "up", "down", "up"]
+    sequence: ["down", "up", "down", "up"],
+    content: `
+
+    `
   }, {
     name: "Upload Data",
-    sequence: ["left", "right", "up", "up", "up"]
+    sequence: ["left", "right", "up", "up", "up"],
+    content: `
+
+    `
   }, {
     name: "Hellbomb",
-    sequence: ["down", "up", "left", "down", "up", "right", "down", "up"]
+    sequence: ["down", "up", "left", "down", "up", "right", "down", "up"],
+    content: `
+
+    `
   }]
 }]
