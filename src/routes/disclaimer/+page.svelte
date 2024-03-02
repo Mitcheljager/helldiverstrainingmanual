a6721c9bc8bedeea238596d90f69a865a5ce2061
Mitchel <mitchel_jager@hotmail.com>
2024-03-02 03:01:28 +0100

    chore: Copy

--- src/routes/disclaimer/+page.svelte
+++ src/routes/disclaimer/+page.svelte
@@ -2,7 +2,7 @@
 
 <p>This website is not officially associated with or endorsed by Helldivers 2 or its developer Arrowhead Game Studios. All trademarks, registered trademarks, product names, and company names or logos mentioned herein are the property of their respective owners.</p>
 
-<p>Any references to Helldivers 2 or Arrowhead Studios are for descriptive purposes only. The content provided on this website is for informational purposes and does not imply any official partnership or endorsement by Helldivers 2 or Arrowhead Studios.</p>
+<p>Any references to Helldivers 2 or Arrowhead Game Studios are for descriptive purposes only. The content provided on this website is for informational purposes and does not imply any official partnership or endorsement by Helldivers 2 or Arrowhead Game Studios.</p>
 
 <p>We strive to provide accurate and up-to-date information, but we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.</p>
 
