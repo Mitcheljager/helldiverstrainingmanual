9c134e673f2a7b5329f1dcc783af6023f7c243f7
Mitchel <mitchel_jager@hotmail.com>
2024-03-03 18:42:34 +0100

    chore: Copy

--- src/routes/api/+page.svelte
+++ src/routes/api/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="mt-1 mb-1/2">API</h1>
+<h1>API</h1>
 
 <p>The API grants you access to real-time battle data, empowering you to spread the word of our noble cause across the galaxy. Use it to share updates, rally allies, and showcase our progress in the fight for democracy. Let's unite the galaxy in our mission to eradicate tyranny and usher in a new era of peace and democracy. Together, we can ignite the flames of liberty across the stars!</p>
 
@@ -9,9 +9,11 @@
   <li><code>/api/v1/war/campaign</code> - Provides a shorter and more ready to handle list of all current active planets</li>
 </ul>
 
-<p>The API is fairly barebones but provides all the info you need to replicate this page and more. There is no authentication or rate limiting. Handle it with respect, soldier.</p>
+<p>The API is fairly barebones but provides all the info you need to replicate the Live War Status page and more.</p>
 
 <p>The data comes from an official endpoint, but it's not an officially open API. It's entirely possible it will stop working at some point.</p>
+
+<p>There is no authentication or rate limiting. Handle it with respect, soldier.</p>
 
 <p>This API is subject to change. Use at your own risk.</p>
 
