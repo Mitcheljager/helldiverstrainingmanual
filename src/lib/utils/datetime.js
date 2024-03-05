82ad9f551925b689d876ea8a5f73f827b0e51aef
Mitchel <mitchel_jager@hotmail.com>
2024-03-05 01:32:11 +0100

    fix: Incorrect clamping for timeFromNow

--- src/lib/utils/datetime.js
+++ src/lib/utils/datetime.js
@@ -2,9 +2,9 @@ export function timeFromNow(unixTimestamp) {
   const currentTime = Math.floor(Date.now() / 1000)
   const totalSeconds =  unixTimestamp - currentTime
 
-  const hours = Math.min(Math.floor(totalSeconds / 3600), 0)
-  const minutes = Math.min(Math.floor(totalSeconds / 60) % 60, 0)
-  const seconds = Math.min(Math.floor(totalSeconds % 60), 0)
+  const hours = Math.max(Math.floor(totalSeconds / 3600), 0)
+  const minutes = Math.max(Math.floor(totalSeconds / 60) % 60, 0)
+  const seconds = Math.max(Math.floor(totalSeconds % 60), 0)
 
   return hours.toString().padStart(2, "0") + ":" + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0")
 }
