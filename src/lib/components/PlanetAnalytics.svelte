9a8e50a8caae97fe7289087c000bd1628bb7c800
Mitchel <mitchel_jager@hotmail.com>
2024-03-06 18:41:35 +0100

    refactor: Move analytics to separate component

---
+++ src/lib/components/PlanetAnalytics.svelte
@@ -0,0 +1,150 @@
+<script>
+  import { LinkedChart, LinkedLabel, LinkedValue } from "svelte-tiny-linked-charts"
+	import { fetchHistory } from "$lib/api/history"
+	import { slide } from "svelte/transition"
+	import { planetNames } from "$lib/data/planets"
+
+  export let index
+
+  function getChartProps(history, players = false) {
+    const data = {}
+    ;[...history].reverse().forEach((item) => {
+      const datetime = new Date(item.created_at)
+      const options = {
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: false,
+      }
+
+      // @ts-ignore
+      const formattedDate = datetime.toLocaleDateString(undefined, options)
+
+      data[formattedDate] = players ? item.player_count : getPlanetPercentage(item)
+    })
+
+    return {
+      data,
+      scaleMax: Math.max(...Object.values(data)) + 0.00001,
+      scaleMin: Math.min(...Object.values(data))
+    }
+  }
+
+  function getPlanetPercentage(data) {
+    const health = data.current_health
+    const maxHealth = data.max_health
+
+    return (100 - (100 / maxHealth * health)).toFixed(4)
+  }
+</script>
+
+<div class="charts">
+  <h4 class="mb-0">{planetNames[index]}</h4>
+
+  {#await fetchHistory(index)}
+    <span transition:slide|global={{ duration: 100 }}>Loading...</span>
+  {:then data}
+    {#if Object.entries(data).length !== 0}
+      {#each [{ header: "Liberation percentage", players: false }, { header: "Number of Helldivers", players: true }] as { header, players }}
+        <div class="chart" transition:slide|global={{ duration: 100 }} >
+          <h5>{header}</h5>
+
+          <LinkedChart
+            width={576}
+            height={150}
+            gap={0}
+            barMinHeight={2}
+            barMinWidth={2}
+            linked="planet"
+            uid={index + header}
+            lineColor="currentColor"
+            fill="var(--chart-color)"
+            {...getChartProps(data, players)} />
+
+          <div class="labels">
+            <div><LinkedValue uid={index + header} transform={(value) => value.toLocaleString() + (players ? "" : "%")} /></div>
+            <div><LinkedLabel linked="planet" /></div>
+          </div>
+        </div>
+      {/each}
+    {/if}
+
+    {#if Object.entries(data).length < 200}
+      <em transition:slide|global={{ duration: 100 }}>
+        {#if Object.entries(data).length === 0}
+          No activity has been recorded on this planet
+        {:else}
+          This data is still populating
+        {/if}
+      </em>
+    {/if}
+  {:catch}
+    Something went wrong when fetching the analytics.
+  {/await}
+</div>
+
+<style lang="scss">
+  h5 {
+    margin: 0 0 $margin * 0.15;
+    color: darken($text-color, 20%);
+    font-weight: normal;
+    font-size: 1rem;
+  }
+
+  .charts {
+    display: flex;
+    flex-direction: column;
+    gap: $margin * 0.25;
+    width: min(20rem, 50vw);
+    height: auto;
+    text-align: center;
+
+    em {
+      font-family: $font-family;
+      font-size: 0.75rem;
+      font-weight: normal;
+      color: $text-color;
+      line-height: 1em;
+    }
+  }
+
+  .chart {
+    position: relative;
+    height: auto;
+    color: $white;
+    font-size: 0.85rem;
+    line-height: 1.45em;
+
+    :global(svg) {
+      display: block;
+      width: 100%;
+      height: auto;
+      max-height: 80px;
+      background: $bg-base;
+
+      @include breakpoint(sm) {
+        max-height: 100vh;
+      }
+    }
+
+    :global(svg *:focus) {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+
+  .labels {
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: $margin * 0.15;
+    width: 100%;
+    font-family: $font-family-alt;
+    font-size: 0.75rem;
+    line-height: 1.45em;
+    pointer-events: none;
+  }
+</style>
