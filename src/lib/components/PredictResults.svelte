22c5954445338f02b6252f16e6e3f28c62f96115
Mitchel <mitchel_jager@hotmail.com>
2024-03-11 14:22:52 +0100

    feat: Refetch history on interval

--- src/lib/components/PredictResults.svelte
+++ src/lib/components/PredictResults.svelte
@@ -2,12 +2,13 @@
 	import { browser } from "$app/environment";
 	import { fetchHistory } from "$lib/api/history";
   import { hoursDifference } from "$lib/utils/datetime"
-	import { onMount } from "svelte"
+	import { onDestroy, onMount } from "svelte"
 
   export let planetIndex
   export let rateDirection = 0
   export let percentage = 0
 
+  let interval
   let estimatedEnd = null
   let average = 0
   let now = Date.now()
@@ -17,43 +18,49 @@
   onMount(async () => {
     if (!browser) return
 
-    const history = await fetchHistory(planetIndex, { limit: 2 })
+    predictResults()
+    interval = setInterval(predictResults, 300_000) // Every 5 minutes
+  })
 
-    calculateTimeTo100(history)
+  onDestroy(() => {
+    if (interval) clearInterval(interval)
   })
 
   function calculateTimeTo100(history) {
-    console.log(history)
     average = history.map(i => 100 - (100 / i.max_health * i.current_health)).reduce((a, b) => a + b) / history.length
 
     const firstEntryTime = history.length > 0 ? new Date(history[history.length - 1].created_at).getTime() : Date.now()
     const interval = Math.max(1, (Date.now() - firstEntryTime) / 1000)
 
-    let rateOfChange = interval > 0 ? (percentage - average) / interval : 0
-    rateOfChange = 10000
+    const rateOfChange = interval > 0 ? (percentage - average) / interval : 0
+
     const remainingPercentage = 100 - percentage
     const timeToFilledInSeconds = remainingPercentage / rateOfChange
 
     const currentTimeInMilliseconds = Date.now()
     const timeToFilledInMilliseconds = currentTimeInMilliseconds + timeToFilledInSeconds * 1000
 
-    estimatedEnd = timeToFilledInMilliseconds > 9e+8 ? Infinity : Math.floor(timeToFilledInMilliseconds / 1000)
-
     const roundedPercentage = parseFloat(percentage.toFixed(4))
     const roundedAverage = parseFloat(average.toFixed(4))
 
+    estimatedEnd = Math.floor(timeToFilledInMilliseconds / 1000)
     rateDirection = roundedAverage === roundedPercentage ? 0 : roundedPercentage > roundedAverage ? 1 : -1
-}
+  }
+
+  async function predictResults() {
+    const history = await fetchHistory(planetIndex, { limit: 2 })
+    calculateTimeTo100(history)
+  }
 </script>
 
 {#if estimatedEnd === null}
   Predicting results...
 {:else if percentage === 0 && average === 0}
   Overwhelming forces await
-{:else if estimatedEnd === Infinity}
+{:else if estimatedEnd === Infinity || estimatedEnd > 9e+9}
   We're in a stalemate
 {:else}
-  {estimatedEnd > (now / 1000) ? "Victory" : "Loss"} in
+  {estimatedEnd > (now / 1000) ? "Liberty" : "Rout"} in
   {#if hoursToGo < 1}
     less than 1 hr
   {:else}
