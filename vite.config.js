e54ce3c14797cf24a64f53d629d8ab14390f0cba
Mitchel <mitchel_jager@hotmail.com>
2024-03-16 03:02:14 +0100

    Revert "chore: Replace sentry adapter"
    This reverts commit 8d8d9bf2d5a063243b00451ba68f3f860e7468c2.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	ssr: {
-		noExternal: ['@jill64/sentry-sveltekit-cloudflare']
-	},
+	plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "mitchel-jager",
+            project: "javascript-sveltekit"
+        }
+    }), sveltekit()],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
 	},
