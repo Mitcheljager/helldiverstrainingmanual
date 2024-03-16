c26c8ae27735387b9f7190ffed44cb7429965f8e
Mitchel <mitchel_jager@hotmail.com>
2024-03-16 02:19:38 +0100

    feat: Add Sentry

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "mitchel-jager",
+            project: "javascript-sveltekit"
+        }
+    }), sveltekit()],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
 	},
