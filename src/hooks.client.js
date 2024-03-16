8d8d9bf2d5a063243b00451ba68f3f860e7468c2
Mitchel <mitchel_jager@hotmail.com>
2024-03-16 02:38:04 +0100

    chore: Replace sentry adapter

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -1,21 +1,3 @@
-import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
-import * as Sentry from '@sentry/sveltekit';
+import { init } from '@jill64/sentry-sveltekit-cloudflare/client'
 
-Sentry.init({
-  dsn: 'https://833fd7ffbb41787467e62104ae116bbf@o4506918033293312.ingest.us.sentry.io/4506918036963328',
-  tracesSampleRate: 1.0,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-  
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [replayIntegration()],
-});
-
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
+init('https://833fd7ffbb41787467e62104ae116bbf@o4506918033293312.ingest.us.sentry.io/4506918036963328')
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "mitchel-jager",
-            project: "javascript-sveltekit"
-        }
-    }), sveltekit()],
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['@jill64/sentry-sveltekit-cloudflare']
+	},
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
 	},
