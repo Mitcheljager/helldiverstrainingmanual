27bd4c7687cc7cd3c1c7a8c7868497d39f8cdb85
Mitchel <mitchel_jager@hotmail.com>
2024-03-19 03:24:15 +0100

    chore: Add hero to equipment page

--- src/routes/equipment/+page.svelte
+++ src/routes/equipment/+page.svelte
@@ -1,9 +1,14 @@
+<script>
+	import Hero from "$lib/components/Hero.svelte"
+</script>
 
 <svelte:head>
   <title>Equipment | Helldivers Training Manual</title>
 </svelte:head>
 
-<h1>Equipment: What separates man from beast</h1>
+<Hero basepath="/images/content" filename="equipment">
+  Equipment: What separates man from beast
+</Hero>
 
 <p>In the unyielding crucible of battle, victory hinges not only on the courage of our hearts and the steel of our resolve but also on the tools and equipment we wield in the heat of combat. Equipment is the lifeblood of our war effort, the very arsenal upon which our triumphs are forged and our enemies vanquished.</p>
 <p>From the humble grenade to the boosters that course through our veins, each piece of equipment serves a vital role in our quest for Liberty. But remember, soldiers, equipment is only as effective as the hands that wield it. It falls upon each and every one of you to master your gear, to harness its power to its fullest potential, and to unleash its fury upon our enemies with unrelenting ferocity.</p>
