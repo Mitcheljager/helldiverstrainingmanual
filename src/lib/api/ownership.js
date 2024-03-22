d1ad8e3cf3bf849078fb8ba97e86dd478036ec91
Mitchel <mitchel_jager@hotmail.com>
2024-03-22 16:38:58 +0100

    feat: Add caching to ownership

--- src/lib/api/ownership.js
+++ src/lib/api/ownership.js
@@ -1,11 +1,19 @@
 import { supabase } from "$lib/db"
+import { addCache, getCache } from "./cache"
 
 export async function fetchOwnership() {
+  const key = "ownership"
+  const cached = await getCache(key, { storeOnly: true })
+
+  if (cached) return cached
+
   const { data, error } = await supabase
     .from("ownership")
     .select("created_at, planet_index, current_owner, previous_owner")
     .order("created_at", { ascending: false })
     .range(0, 1000)
+
+  addCache(key, data, 3600000, { storeOnly: true }) // 1 Hour
 
   if (error) throw new Error(error.message)
 
@@ -13,12 +21,19 @@ export async function fetchOwnership() {
 }
 
 export async function fetchOwnershipForPlanet(index) {
+  const key = `ownership_${index}`
+  const cached = await getCache(key, { storeOnly: true })
+
+  if (cached) return cached
+
   const { data, error } = await supabase
     .from("ownership")
     .select("created_at, planet_index, current_owner, previous_owner")
     .order("created_at", { ascending: false })
     .eq("planet_index", index)
     .range(0, 1000)
+
+  addCache(key, data, 600000, { storeOnly: true }) // 10 Minutes
 
   if (error) throw new Error(error.message)
 
