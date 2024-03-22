b0ad8c4a63fd29222df15a2ca06b9e74113ab643
Mitchel <mitchel_jager@hotmail.com>
2024-03-22 21:50:00 +0100

    chore: Remove left over console.log

--- src/lib/api/api.js
+++ src/lib/api/api.js
@@ -1,7 +1,6 @@
 export async function api(path) {
   try {
     const response = await fetch(`/api/v1/${path}`)
-    console.log(response)
     const parsed = await response.json()
 
     return parsed
