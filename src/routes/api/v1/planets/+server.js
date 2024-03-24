77b4d2d5b6f55ea7080b2de7e7d57defa656309f
Mitchel <mitchel_jager@hotmail.com>
2024-03-24 04:07:55 +0100

    chore: Increase planets api endpoint cache ttl

--- src/routes/api/v1/planets/+server.js
+++ src/routes/api/v1/planets/+server.js
@@ -2,7 +2,7 @@ import { planetData } from "$lib/data/planets"
 import { getApiHeaders } from "$lib/utils/headers"
 
 export async function GET() {
-  const headers = getApiHeaders(300)
+  const headers = getApiHeaders(3600)
 
   const data = {}
   Object.entries(planetData).forEach(([key, { name, sector, biome, environmentals }]) => {
